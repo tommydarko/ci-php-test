946dd6368324345f4332dec82cbf77b758f52466
Tom Barrett <tom.barrett@ceros.com>
2012-07-18 12:57:01 +0100

    Added some bad code to see if it is detected

--- lib/Util.php
+++ lib/Util.php
@@ -3,16 +3,20 @@
 class Util {
 	public function isEven($num) {
 		if ($num & 1) {
+			print "Is an even number";
 			return true;
 		} else {
+			print "Is an odd number";
 			return false; 
 		}
 	}
 	
 	public function isEvenAgain($num) {
 		if ($num & 1) {
+			print "Is an even number";
 			return true;
 		} else {
+			print "Is an odd number";
 			return false; 
 		}
 	}
