4fb476d459fb000e18d8b1153c2be1e3064735d8
Tom Barrett <tom.barrett@ceros.com>
2012-07-18 13:01:37 +0100

    Added some bad code to see if it is detected

--- lib/World.php
+++ lib/World.php
@@ -8,11 +8,15 @@ class World extends Hello {
 	}
 	
 	public function isEven($num) {
+		if (! is_int($num)) {
+			throw new Exception("Please provide an Int", 1);
+		}
 		if ($num & 1) {
+			print "Is an even number";
 			return true;
 		} else {
+			print "Is an odd number";
 			return false; 
 		}
 	}
-	
 }
