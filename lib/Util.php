6b603147f52fe21f7662c133d0cd06d2fe1b93b3
Tom Barrett <tom.barrett@ceros.com>
2012-07-18 11:50:14 +0100

    Added some bad code to see if it is detected

---
+++ lib/Util.php
@@ -0,0 +1,21 @@
+<?php
+
+class Util {
+	public function isEven($num) {
+		if($num & 1) {
+			return true;
+		} else {
+			return false; 
+		}
+	}
+	
+	public function badFunction($pram) {
+		if (true) {
+			return true; 
+		} else {
+			return false; 
+		}
+		
+		return false;
+	}
+}
