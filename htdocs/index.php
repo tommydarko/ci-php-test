fcf204c4bf0448f91a1d7e8354728a6cacb2eb3d
Tom Barrett <tom.barrett@ceros.com>
2012-07-19 13:50:59 +0100

    index.php is now dependant on sub mods

--- htdocs/index.php
+++ htdocs/index.php
@@ -2,7 +2,13 @@
 	require_once '../lib/World.php';
 	require_once '../lib/Util.php';
 	
+	require_once '../vendor/submodule-test-01/Submodule01.php';
+	require_once '../vendor/submodule-test-02/Submodule02.php';
+	
 	$world = new World();
+	
+	$s1 = new Submodule01();
+	$s2 = new Submodule02();
 ?>
 <!DOCTYPE html>
 <html>
@@ -13,5 +19,11 @@
 	<body>
 		<h1><?php print $world->getMessage(); ?></h1>
 		<h2 id="clickMe">Click here for a message from a Require JS module.</h2>
+		<hr />
+		<h1>Submodule Info:</h1>
+		<ol>
+			<li>01 Version: <?php print $s1->getVersion(); ?></li>
+			<li>02 Version: <?php print $s2->getVersion(); ?></li>
+		</ol>
 	</body>
 </html>
